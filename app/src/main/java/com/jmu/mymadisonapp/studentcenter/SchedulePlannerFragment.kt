07b702adf9c50d2e3ca024e106317b386224fdee
batfolx <47933665+batfolx@users.noreply.github.com>
2019-05-29 16:59:47 -0400

    Add files via upload

---
+++ app/src/main/java/com/jmu/mymadisonapp/studentcenter/SchedulePlannerFragment.kt
@@ -0,0 +1,23 @@
+package com.jmu.mymadisonapp.studentcenter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jmu.mymadisonapp.R
+
+class SchedulePlannerFragment : Fragment()
+{
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+         inflater.inflate(R.layout.fragment_scheduleplanner, container, false).apply{
+
+         }
+
+
+
+
+
+
+}
